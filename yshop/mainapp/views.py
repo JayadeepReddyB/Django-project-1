6335ab590d414574111f8818b6eb7af1cecf5348
JayadeepReddyB <jayadeepreddy422@gmail.com>
2025-01-27 11:01:34 +0530

    Initial project setup

---
+++ yshop/mainapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect # helps to render the template , redirect the user if required 
+from django.http import HttpResponse # helps us to create a http response object -- responding to the user
+
+from django.template import loader # helps in file handling -- handling thee file 
+from .models import Product
+
+# Create your views here.
+
+def home(request):
+    products = Product.objects.all() # querying all records in the DB of entity type `Product`
+    # i.e. this translates to the DQL :-> `SELECT * FROM PRODUCT;`
+    context = {
+        'prods' : products # the key `prods` will now be available  to use in the django template design
+
+    }    # context dictionary for passing data for rendering
+    template = loader.get_template('home.html') # creating a template object from the designed template html
+    return HttpResponse(template.render(context, request))  # creates a response object after rendering
+    # the returned response has the html of completed webpage including required data.
